e03fbbf881238829f943a64b7dc55e353bec2bb2
Coliiin1 <carlosdanielavilacolin@gmail.com>
2025-09-17 11:19:47 -0600

    Se suben variables

---
+++ ProyectoPOO/src/MAIN/Items.java
@@ -0,0 +1,13 @@
+package MAIN;
+
+public class Items {
+    public String NombreProd, Marca;
+    public float Precio;
+    public short Cantidad;
+    public String Talla;
+    public String Sexo;
+    public String Proveedor;
+    public String CodigoProd;
+    public float Oferta;
+    public String DirImagen;
+}
---
+++ ProyectoPOO/src/MAIN/Usuario.java
@@ -0,0 +1,13 @@
+package MAIN;
+
+public class Usuario {
+    private String Usuario;
+    private String Nombres, APaterno, AMaterno;
+    private byte Dia, Mes;
+    private short Anio;
+    private String Contrasenia;
+    
+    public static void Consultar(){
+        
+    }
+}
