7ca76f10eb715bd5883ddfc5f91165c074b85add
FAXZ4444 <fabrigamerproxd@gmail.com>
2025-10-20 07:43:24 -0600

    SE quita legacy

--- ProyectoPOO/src/MAIN/ProyectoPOO.java
+++ ProyectoPOO/src/MAIN/ProyectoPOO.java
@@ -1,50 +1,10 @@
 package MAIN;
-import Control.Items;
-import Control.Administrador;
-import Control.Cliente;
-import Control.Usuario;
-import GUI.*;
-import java.util.Scanner;
+import Control.Controlador;
 
 
 public class ProyectoPOO {
 
     public static void main(String[] args) {
-        Usuario user;
-        byte opc=0;
-        byte cont=0;
-        Scanner in=new Scanner(System.in);
-        Interfaz inicio=new Interfaz();
-        IniciodeSesion init=new IniciodeSesion();
-        init.setVisible(true);
-        System.out.println("DESEA INICIAR SESION? (1/0)");
-        opc=in.nextByte();
-        
-        
-        if(opc==1){
-            user=new Administrador("ADMIN","12345");
-            inicio.iniciarSesion(user.Usuario,user.Contrasenia);
-        }else{
-            user=new Cliente(null,null);//es un cliente default
-        }
-        Items[] almacen=new Items[50];
-        Items item=new Items();
-        
-        while(opc!=4){
-            opc=inicio.menu();
-            switch(opc){
-                case 1:
-                    almacen[cont]=user.Agregar();
-                    cont++;
-                    break;
-                case 2:
-                    item=user.Consultar(almacen);
-                    item=null;
-                    break;
-                case 3:    
-                    user.Eliminar(almacen);
-            }
-        }
-    }
-    
+        Controlador control=new Controlador();
+    } 
 }
